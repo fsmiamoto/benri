8dd6ffe3d8d4daf4919030b8c79c0c04c9f64b17
Francisco <fsmiamoto@gmail.com>
2020-06-12 12:57:44 -0300

    feat: use duration module

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/fsmiamoto/benri/modules/directory"
+	"github.com/fsmiamoto/benri/modules/duration"
 	"github.com/fsmiamoto/benri/modules/git"
 	"github.com/fsmiamoto/benri/prompt"
 )
 
 var modules = []*prompt.Module{
-	{directory.CurrentWorking, []color.Attribute{color.FgBlue, color.Bold}},
-	{git.CurrentBranch, []color.Attribute{color.FgMagenta, color.Bold}},
-	{git.Status, []color.Attribute{color.FgCyan, color.Bold}},
+	{Source: directory.CurrentWorking, ColorOptions: []color.Attribute{color.FgBlue, color.Bold}},
+	{Source: git.CurrentBranch, ColorOptions: []color.Attribute{color.FgMagenta, color.Bold}},
+	{Source: duration.Seconds, ColorOptions: []color.Attribute{color.FgYellow, color.Bold}},
 }
 
 func main() {
