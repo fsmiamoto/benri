ddc6cd897c98b03d8fc6ad0711f348eb1ff0a975
Francisco <fsmiamoto@gmail.com>
2020-06-11 23:17:29 -0300

    refactor: create modules directory

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 
 	"github.com/fatih/color"
-	"github.com/fsmiamoto/benri/directory"
-	"github.com/fsmiamoto/benri/git"
+	"github.com/fsmiamoto/benri/modules/directory"
+	"github.com/fsmiamoto/benri/modules/git"
 	"github.com/fsmiamoto/benri/prompt"
 )
 
 var modules = []*prompt.Module{
 	{directory.CurrentWorking, []color.Attribute{color.FgBlue, color.Bold}},
 	{git.CurrentBranch, []color.Attribute{color.FgMagenta, color.Bold}},
-	{git.Status, []color.Attribute{color.FgRed, color.Bold}},
+	{git.Status, []color.Attribute{color.FgCyan, color.Bold}},
 }
 
 func main() {
-	fmt.Print(prompt.New(modules), "\n")
+	fmt.Print(prompt.New(modules), "\n", "$ ")
 }
