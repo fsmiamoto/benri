a89a1b24ad6ea3492b9e454b2236ea862cc523a9
Francisco Miamoto <fsmiamoto@gmail.com>
2020-06-19 21:05:14 -0300

    feat: add hostname module

---
+++ modules/hostname/hostname.go
@@ -0,0 +1,27 @@
+package hostname
+
+import (
+	"os"
+)
+
+// WithUser returns a string with the user and hostname
+// e.g user@machin
+func WithUser() string {
+	host, err := os.Hostname()
+	if err != nil {
+		return ""
+	}
+
+	user := os.Getenv("USER")
+
+	return user + "@" + host
+}
+
+// String returns the hostname as reported by the kernel
+func String() string {
+	host, err := os.Hostname()
+	if err != nil {
+		return ""
+	}
+	return host
+}
