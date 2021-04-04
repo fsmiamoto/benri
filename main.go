2c23df97da66ce5ffaa3a0ae07739450f84a9448
Francisco Miamoto <fsmiamoto@gmail.com>
2021-04-03 21:11:33 -0300

    feat: change base layout

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ import (
 )
 
 var onLeft = []*m.Module{
+	m.New(hostname.WithUser, m.After(" "), m.ColorOptions(color.FgHiCyan, color.Bold)),
 	m.New(directory.CurrentWorking, m.After(" "), m.ColorOptions(color.FgBlue, color.Bold)),
+}
+
+var onRight = []*m.Module{
+	m.New(duration.WithDisplayMinInSecs(3), m.After(" "), m.ColorOptions(color.FgYellow, color.Bold)),
 	m.New(git.CurrentBranch, m.After(" "), m.ColorOptions(color.FgMagenta, color.Bold)),
 	m.New(git.HasModified("!"), m.ColorOptions(color.FgHiGreen, color.Bold)),
 	m.New(git.HasUntracked("?"), m.ColorOptions(color.FgHiBlue, color.Bold)),
 	m.New(git.HasStaged("+"), m.ColorOptions(color.FgHiRed, color.Bold)),
 	m.New(git.HasStashed("$"), m.ColorOptions(color.FgHiMagenta, color.Bold)),
-}
-
-var onRight = []*m.Module{
-	m.New(duration.WithDisplayMinInSecs(3), m.After(" "), m.ColorOptions(color.FgYellow, color.Bold)),
-	m.New(hostname.WithUser, m.After(" "), m.ColorOptions(color.FgHiCyan, color.Bold)),
-	m.New(clock.With24H, m.ColorOptions(color.FgHiWhite, color.Bold), m.After(" ")),
+	m.New(clock.With24H, m.Before(" "), m.ColorOptions(color.FgHiWhite, color.Bold), m.After(" ")),
 }
 
 func main() {
